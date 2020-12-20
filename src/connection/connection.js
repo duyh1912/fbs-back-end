96face39604f98befb13b0bad1e3f906a51ed97d
haidong1311999 <68586609+haidong1311999@users.noreply.github.com>
2020-12-20 17:37:17 +0700

    update image
    updata image

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,14 +1,8 @@
 var mongoose = require('mongoose');
 
 
-<<<<<<< HEAD
 // // var URL = 'mongodb+srv://vnfood:vnfood@cluster0.amioj.mongodb.net/adminfbs?retryWrites=true&w=majority';
 // var URL = 'mongodb+srv://vnfood:vnfood@cluster0-amioj.mongodb.net/vnfood?retryWrites=true&w=majority';
-=======
-// var URL = 'mongodb+srv://vnfood:vnfood@cluster0.amioj.mongodb.net/adminfbs?retryWrites=true&w=majority';
-// var URL = 'mongodb+srv://vnfood:vnfood@cluster0-amioj.mongodb.net/vnfood?retryWrites=true&w=majority';
-var URL = 'mongodb+srv://admin:admin@cluster0.qvjxs.mongodb.net/fbs?retryWrites=true&w=majority';
->>>>>>> 12f8686d1af52e699f090a13711af6400e5defa0
 
 // mongodemo
 var URL = 'mongodb+srv://admin:admin@cluster0.qvjxs.mongodb.net/fbs?retryWrites=true&w=majority';
