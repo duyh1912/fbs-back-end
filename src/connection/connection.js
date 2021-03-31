5af501a054bb41c7eb7e0c8b9f391f2f7c352402
tringuyen <trinm1782@gmail.com>
2021-03-31 20:21:20 +0700

    link2

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -5,8 +5,9 @@ var mongoose = require('mongoose');
 // var URL = 'mongodb+srv://vnfood:vnfood@cluster0-amioj.mongodb.net/vnfood?retryWrites=true&w=majority';
 
 // mongodemo
-var URL = 'mongodb+srv://admin:admin@cluster0.qvjxs.mongodb.net/fbs?retryWrites=true&w=majority';
+// var URL = 'mongodb+srv://admin:admin@cluster0.qvjxs.mongodb.net/fbs?retryWrites=true&w=majority';
 
+var URL = 'mongodb+srv://admin:<password>@cluster0.qvjxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.set('useFindAndModify', false);
 
 //Connection establishment
